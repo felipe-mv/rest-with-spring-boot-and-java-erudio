f37cd53bc39727ad0a3a46c53c582328317e08fb
felipe varsone maculan <67004783+felipe-mv@users.noreply.github.com>
2022-08-03 14:41:00 -0300

    seção 07

---
+++ 05_FirstStepsInJavawithSpring/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/innmanager/math/SimpleMath.java
@@ -0,0 +1,29 @@
+package br.com.innmanager.math;
+
+public class SimpleMath {
+	
+
+	public Double sum(Double numberOne, Double numberTwo)  {				
+		return numberOne + numberTwo;
+	}
+	
+	public Double sub(Double numberOne, Double numberTwo)  {		
+		return numberOne - numberTwo;
+	}
+	
+	public Double mult (Double numberOne, Double numberTwo)  {	
+		return numberOne * numberTwo;
+	}
+	
+	public Double div(Double numberOne, Double numberTwo)  {				
+		return numberOne + numberTwo;
+	}
+	
+	public Double med(Double numberOne, Double numberTwo)  {		
+		return (numberOne + numberTwo) / 2;		
+	}	
+	
+	public Double raiz(Double numberOne)  {
+		return Math.sqrt(numberOne);
+	}
+}
